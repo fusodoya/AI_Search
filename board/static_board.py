039d10b1a933995b340d9bfa35c8b99e2c46832f
fusodoya <fusodoya@gmail.com>
2024-11-07 10:25:18 +0700

    add UI

--- board/static_board.py
+++ board/static_board.py
@@ -7,12 +7,12 @@ class StaticBoard(BoardInterface):
         super().__init__(initial_board)
         for h in range(self.height):
             for w in range(self.width):
-                if self._board[h][w] in {BoardSymbol.SWITCH.value,
+                if self.board[h][w] in {BoardSymbol.SWITCH.value,
                 BoardSymbol.STONE_ON_SWITCH.value, 
                 BoardSymbol.ARES_ON_SWITCH.value}:
-                    self._board[h][w] = BoardSymbol.SWITCH.value
-                elif (self._board[h][w] != BoardSymbol.WALL.value):
-                    self._board[h][w] = BoardSymbol.FREE_SPACE.value
+                    self.board[h][w] = BoardSymbol.SWITCH.value
+                elif (self.board[h][w] != BoardSymbol.WALL.value):
+                    self.board[h][w] = BoardSymbol.FREE_SPACE.value
     
     def print_board(self) -> None:
         print("Static board (include wall, free space, and switch):")
